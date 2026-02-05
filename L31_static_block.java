267b388e4663460e2aed05f62ab923b176f6c5ba
Kwamz3 <charlie18appiah@gmail.com>
2026-02-05 23:29:28 +0000

    Add comments to clarify static block behavior and update main method for class loading demonstration

--- L31_static_block.java
+++ L31_static_block.java
@@ -4,6 +4,7 @@ class Mobile {
     int price;
     static String name;
 
+    // whenever you call the class the static block is loaded first then the constructor is loaded after the object follows
     static {
         name = "Phone";
         System.out.println("In static block");
@@ -24,13 +25,16 @@ class Mobile {
 }
 
 public class L31_static_block {
-    public static void main(String[] args) {
-        Mobile obj1 = new Mobile();
-        obj1.brand = "Apple";
-        obj1.price = 1500;
-        Mobile.name = "Phone";
+    public static void main(String[] args) throws ClassNotFoundException {
+        // Mobile obj1 = new Mobile();
+        // obj1.brand = "Apple";
+        // obj1.price = 1500;
+        // Mobile.name = "Phone";
         
-        Mobile obj2 = new Mobile();
+        // Mobile obj2 = new Mobile();
+
+        // to just load the class without loading the constructor we use the class Class.forname
+        Class.forName("Mobile");
 
     }
 }
